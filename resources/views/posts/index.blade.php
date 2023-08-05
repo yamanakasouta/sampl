36d00942f2926d1b4b295710e1a9cc22cc208f4d
Sota-Yamanaka <y.sota.tf0429@icloud.com>
2023-08-05 16:09:07 +0900

    カリキュラム8-2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,20 +4,20 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <a href="/posts/{{ $post->id }}<h2 class='title'>{{ $post->title }}</h2></a>
+                    <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
         <div class='paginate'>
-            {{ $posts->links () }}
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
