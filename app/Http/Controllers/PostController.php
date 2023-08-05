5412d68adee8a4f489bb5dc665b7623d89bb9a44
Sota-Yamanaka <y.sota.tf0429@icloud.com>
2023-08-05 15:46:02 +0900

    カリキュラム８−2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     
     public function show(Post $post)
 {
-    return view('posts.show')->with(['post' => $post]);
+    return view('posts/show')->with(['post' => $post]);
     //
 }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,12 +5,12 @@
         <title>Blog</title>
         
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"rel="stylesheet">
         
     </head>
     <body class="antialiased">
         <h1 class='title'>
-            {{　$post->title　}}
+            {{ $post->title }}
         </h1>
         <div class='content'>
                 <div class='content_post'>
