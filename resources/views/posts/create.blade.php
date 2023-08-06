bfbad1574137f8505591ff1e3764acc4a8e644f7
Sota-Yamanaka <y.sota.tf0429@icloud.com>
2023-08-06 16:35:13 +0900

    カリキュラム8−4

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,16 +9,20 @@
         
     </head>
     <body class="antialiased">
-        <h1 class='title'>
-            {{ $post->title }}
-        </h1>
-        <div class='content'>
-                <div class='content_post'>
-                    <h3>本文</h3>
-                    <p class='body'>{{ $post->body }}</p>
-                </div>
-        </div>
-        <div class='footer'>
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+            </div>
+            <input type="submit" value="store"/>
+        </form>
+        <div class="footer">
             <a href="/">戻る</a>
         </div>
     </body>
