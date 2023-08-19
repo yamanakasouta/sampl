8013dc05aacefcd42806a515b9509c32741b00e1
Sota-Yamanaka <y.sota.tf0429@icloud.com>
2023-08-19 16:36:52 +0900

    カリキュラム9-1

--- database/migrations/2023_07_29_031454_create_posts_table.php
+++ database/migrations/2023_07_29_031454_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
